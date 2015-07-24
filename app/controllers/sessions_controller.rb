81aaa3854137006ed96f7600bce8edca47630846
Tom <tom_t_lee@yahoo.com>
2015-07-24 15:52:21 +0000

    Create sessions#new page and action. Start sessions#create action and spec

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,17 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:email])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      flash[:success] = "Welcome #{user.full_name}! You are signed in."
+      redirect_to home_path
+    else
+      flash[:danger] = "Incorrect email or password."
+      redirect_to sign_in_path
+    end
+  end
+
+end
---
+++ spec/controllers/sessions_controller_spec.rb
@@ -0,0 +1,45 @@
+require "spec_helper"
+
+describe SessionsController do
+  describe "POST create" do
+    context "with valid credentials" do
+      it "puts the user in the session" do
+        user1 = Fabricate(:user)
+        post :create, email: user1.email, password: user1.password
+        expect(session[:user_id]).to eq(user1.id)
+      end
+
+      it "sets the success notice" do
+        user1 = Fabricate(:user)
+        post :create, email: user1.email, password: user1.password
+        expect(flash[:success]).to be_present
+      end
+
+      it "redirects to the home page" do
+        user1 = Fabricate(:user)
+        post :create, email: user1.email, password: user1.password
+        expect(response).to redirect_to home_path
+      end
+    end
+
+    context "with invalid credentials" do
+      it "does not put the user in the session" do
+        user1 = Fabricate(:user, password: "password")
+        post :create, email: user1.email, password: "abcdefg"
+        expect(session[:user_id]).to be_nil
+      end
+
+      it "sets the error notice" do
+        user1 = Fabricate(:user, password: "password")
+        post :create, email: user1.email, password: "abcdefg"
+        expect(flash[:danger]).to be_present
+      end
+
+      it "redirects to the sign in page" do
+        user1 = Fabricate(:user, password: "password")
+        post :create, email: user1.email, password: "abcdefg"
+        expect(response).to redirect_to sign_in_path
+      end
+    end
+  end
+end
