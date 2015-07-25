4d07b5a56519446e910f4cd561db9323b8c5793c
Tom Lee <tom_t_lee@yahoo.com>
2015-07-25 14:04:21 -0400

    Update syntax per houndCI

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,10 +4,11 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(email: params[:email])
-    if user && user.authenticate(params[:password])
-      session[:user_id] = user.id
-      flash[:success] = "Welcome #{user.full_name}! You are signed in."
+    @user = User.find_by(email: params[:email])
+
+    if user_entered_correct_password?
+      add_user_to_session
+      flash[:success] = "Welcome #{@user.full_name}! You are signed in."
       redirect_to home_path
     else
       flash[:danger] = "Incorrect email or password."
@@ -18,5 +19,15 @@ class SessionsController < ApplicationController
   def destroy
     session[:user_id] = nil
     redirect_to root_path
+  end
+
+  private
+
+  def user_entered_correct_password?
+    @user && @user.authenticate(params[:password])
+  end
+
+  def add_user_to_session
+    session[:user_id] = @user.id
   end
 end
