e9d96df66cd358fd3c3180440ce5971d80095573
Tom <tom_t_lee@yahoo.com>
2015-07-29 19:53:26 +0000

    Continue on photo uploaders

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,14 +1,8 @@
 class ImageUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
 
-  storage :file
-
   version :thumb do
     process resize_to_fill: [200, 200]
-  end
-
-  def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
   def filename
